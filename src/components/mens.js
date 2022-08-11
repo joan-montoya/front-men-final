d52e7e45f88710f3be6c59c79ab66eac421bec88
oscar <org734664@gmail.com>
2022-08-11 00:26:58 -0500

    feat:The mens.js file is completely created and put to work

--- src/components/mens.js
+++ src/components/mens.js
@@ -11,3 +11,23 @@ function Mens() {
       setRegistrado(true);
     }
   };
+
+  return (
+    <div className="cont">
+      <div className="container2">
+        <h1 className="tit">Ingresa tu nombre para ir al chat</h1>
+        {!registrado && (
+        <form onSubmit={registrar}>
+          <input className="input" placeholder="Nombre de usuario" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+          <button className="buton">Ir al chat</button>
+        </form>
+      )}
+
+      {registrado && <Chat nombre={nombre} />}
+      </div>
+      
+    </div>
+  );
+}
+
+export default Mens;
