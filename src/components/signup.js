29ac5f0e0328d5df1b4db2698f99af3691703fad
Joan Montoya <joan15montoya@gmail.com>
2022-08-11 00:07:21 -0500

    feat: signup component completed localhost

--- src/components/signup.js
+++ src/components/signup.js
@@ -12,6 +12,67 @@ export default class SignUp extends Component {
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  return (
-  );
+  handleSubmit(e) {
+    e.preventDefault();
+    const { fname, lname, email, password } = this.state;
+    console.log(fname, lname, email, password);
+    fetch("https://chat--i.herokuapp.com/register", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        fname,
+        email,
+        lname,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userRegister");
+      });
+  }
+  render() {
+    return (
+      <div className="cont">
+        <form onSubmit={this.handleSubmit}>
+        <div className="container"> 
+        <h3>REGISTRARSE</h3>
+            <div>
+                <input type="text" className="input" 
+                placeholder="First name" onChange={(e) => 
+                this.setState({ fname: e.target.value })}/>
+            </div>
+            <div >
+                <input type="text" className="input" 
+                placeholder="Last name" onChange={(e) => 
+                this.setState({ lname: e.target.value })}/>
+            </div>
+            <div>
+                <input className="input" type="email" 
+                placeholder="Enter email"onChange={(e) => 
+                this.setState({ email: e.target.value })}/>
+            </div>
+            <div>
+                <input className="input"
+                type="password"
+                placeholder="Enter password"
+                onChange={(e) => this.setState({ password: e.target.value })}/>
+            </div>
+            <div>
+                <button type="submit" className="buton"> Registrate
+                </button>
+            </div>
+                <p>
+                    <a href="/sign-in">¿Ya tienes cuenta?</a>
+                </p>
+            </div>
+        </form>
+      </div>
+    );
+  }
 }
