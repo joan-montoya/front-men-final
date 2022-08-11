919e7f0ba492949809fb24e83f86c6efa55ae391
oscar <org734664@gmail.com>
2022-08-11 00:26:30 -0500

    feat:The mens.js file is created and the libraries are imported

---
+++ src/components/mens.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Chat from "./Chat";
+
+function Mens() {
+  const [nombre, setNombre] = useState("");
+  const [registrado, setRegistrado] = useState(false);
+
+  const registrar = (e) => {
+    e.preventDefault();
+    if (nombre !== "") {
+      setRegistrado(true);
+    }
+  };
