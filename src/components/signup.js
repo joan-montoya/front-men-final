1884496805203aaa1865be31f29176b095ebcab1
Joan Montoya <joan15montoya@gmail.com>
2022-08-11 00:04:33 -0500

    feat: signup class creation

---
+++ src/components/signup.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./chat.css";
+
+export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      email: "",
+      password: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  return (
+  );
+}
